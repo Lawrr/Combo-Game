dd91c26c4d18f1a395de8c896aa50d9c9deab49a
Syurba <Lawrence1995@hotmail.com>
2015-01-30 05:03:34 +1100

    Initial commit

---
+++ desktop/src/com/syurba/combogame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.syurba.combogame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.syurba.combogame.ComboGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ComboGame(), config);
+	}
+}
---
+++ html/src/com/syurba/combogame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.syurba.combogame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.syurba.combogame.ComboGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ComboGame();
+        }
+}
