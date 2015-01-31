34f118f2639fcc6c2d88bd34f27fcc27d98d0163
Syurba <Lawrence1995@hotmail.com>
2015-01-31 19:26:24 +1100

    Fixed incorrect comment

--- core/src/com/syurba/combogame/InputTransform.java
+++ core/src/com/syurba/combogame/InputTransform.java
@@ -1,7 +1,7 @@
 package com.syurba.combogame;
 
 public class InputTransform {
-    // Transforms x and y coords (which start from the top right) to proper libgdx coords (which start from the bottom left)
+    // Transforms x and y coords (which start from the top left) to proper libgdx coords (which start from the bottom left)
     public static float getCursorToModelX (int x) {
         return (((float)x) * ComboGame.screenWidth) / ((float)ComboGame.screenWidth);
     }
